709dd36a5e0462a5725e34a805bce5e6066e62d6
GeroGili <geronimo.giliberti@davinci.edu.ar>
2024-11-11 20:44:54 -0300

    Login admin

--- src/Admin.java
+++ src/Admin.java
@@ -16,15 +16,18 @@ public class Admin extends Usuario {
 		NroAdmin = nroAdmin;
 	}
 	public static Admin login() {
-        String nombre = JOptionPane.showInputDialog("Ingrese el nombre del administrador:");
-        String dni = JOptionPane.showInputDialog("Ingrese el DNI del administrador:");
-        String contrasena = JOptionPane.showInputDialog("Ingrese la contraseña del administrador:");
-        int nroAdmin = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número de administrador:"));
+        String nombre =  validarCaracteres(JOptionPane.showInputDialog("Ingrese el nombre del administrador:"));
+        String dni =  validarCaracteres(JOptionPane.showInputDialog("Ingrese el DNI:"));
+        String contrasena = validarCaracteres(JOptionPane.showInputDialog("Ingrese la contraseña:")) ;
+        int nroAdmin = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número de administrador:")) ;
         
-        Admin admin = new Admin(nombre, dni, contrasena, nroAdmin);     
+        Admin admin = new Admin(nombre, dni, contrasena, nroAdmin);  
+        Usuario.getAdministradores().add(admin);
+        
         return admin;
         
 	}
+	
         
 	
 	
--- src/Usuario.java
+++ src/Usuario.java
@@ -1,5 +1,7 @@
 import java.util.LinkedList;
 
+import javax.swing.JOptionPane;
+
 public class Usuario {
 	private String nombre;
 	private String dni;
@@ -61,6 +63,33 @@ public class Usuario {
 
 	public static void setAdministradores(LinkedList<Admin> administradores) {
 		Usuario.administradores = administradores;
+	}
+	public static int validarNumeros(String mensaje) {
+		boolean flag;
+		String num = "";
+		do {
+			flag = true;
+			num = JOptionPane.showInputDialog(mensaje);
+			while (num.isEmpty()) {
+				num = JOptionPane.showInputDialog(mensaje);
+			}
+			for (int i = 0; i < num.length(); i++) {
+				if (!Character.isDigit(num.charAt(i))) {
+					flag = false;
+					break;
+				}
+			}
+		} while (!flag);
+
+		return Integer.parseInt(num);
+	}
+
+	public static String validarCaracteres(String mensaeje) {
+		String palabra = "";
+		while (palabra.equals("")) {
+			palabra = JOptionPane.showInputDialog(mensaeje);
+		}
+		return palabra;
 	}
 
 	@Override
