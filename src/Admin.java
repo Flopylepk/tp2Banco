846b0f87cf9d124c2bd839fcf0bfef74aca27e19
GeroGili <geronimo.giliberti@davinci.edu.ar>
2024-11-11 21:34:42 -0300

    arranque registro cliente

--- src/Admin.java
+++ src/Admin.java
@@ -19,7 +19,7 @@ public class Admin extends Usuario {
         String nombre =  validarCaracteres(JOptionPane.showInputDialog("Ingrese el nombre del administrador:"));
         String dni =  validarCaracteres(JOptionPane.showInputDialog("Ingrese el DNI:"));
         String contrasena = validarCaracteres(JOptionPane.showInputDialog("Ingrese la contraseña:")) ;
-        int nroAdmin = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número de administrador:")) ;
+        int nroAdmin = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número de administrador:"));
         
         Admin admin = new Admin(nombre, dni, contrasena, nroAdmin);  
         Usuario.getAdministradores().add(admin);
@@ -27,6 +27,30 @@ public class Admin extends Usuario {
         return admin;
         
 	}
+	  public void registrarCliente() {
+	        String nombre =validarCaracteres(JOptionPane.showInputDialog("Ingrese el nombre del cliente:")) ;
+	        String dni = validarCaracteres(JOptionPane.showInputDialog("Ingrese el DNI del cliente:"));
+	        String contrasena = validarCaracteres(JOptionPane.showInputDialog("Ingrese la contraseña del cliente:"));
+	        String tipo = validarCaracteres(JOptionPane.showInputDialog("ingrese el tipo de cliente"));
+
+
+	        Cliente cliente = new Cliente(nombre, dni, contrasena, tipo, null);
+
+
+	        for (Cliente clienteExistente : Usuario.getClientes()) {
+	            if (clienteExistente.getNombre().equalsIgnoreCase(cliente.getNombre()) && clienteExistente.getDni().equals(cliente.getDni())) {
+	                JOptionPane.showMessageDialog(null, "El cliente  " + cliente.getNombre() + " ya está registrado.");
+	                return;
+	            }
+	        } 
+				
+	        Usuario.getClientes().add(cliente);
+	        JOptionPane.showMessageDialog(null, "Cliente registrado exitosamente: " + cliente.getNombre());
+			
+
+
+	    }
+	
 	
         
 	
